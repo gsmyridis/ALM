43636091f05a21e27125208a5ebe950a1ff95eb1
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-09-24 19:38:10 +0200

    Update

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import datetime
+import numpy as np
+from datetime import datetime, timedelta
 
 
 def load_portfolio():
@@ -21,9 +22,7 @@ def load_portfolio():
     :col payment_freq: repayment frequency in number of months
     :col yieldcurve: identifier of the interest rate curve
     """
-    # Read CSV file
     portfolio = pd.read_csv('data/portfolio.csv')
-    # Turn date columns to DateTime type
     portfolio['issue'] = pd.to_datetime(portfolio['issue'])
     portfolio['maturity'] = pd.to_datetime(portfolio['maturity'])
     return portfolio
@@ -38,11 +37,9 @@ def load_market():
     :col rate: value of the rate in basis points
     :col comment: label of the yield curve tensor
     """ 
-    # Read CSV file
     market = pd.read_csv('data/market.csv')
-    # Turn date columns to DateTime type
     market['date'] = pd.to_datetime(market['date'])
-    market['rate'] = market['rate'].div(100)
+    market['rate'] = market['rate']
     return market
 
 
@@ -53,7 +50,7 @@ def load_data():
 
 def set_now():
     """Sets the now date time 30/09/2014"""
-    return datetime.datetime(2014, 9, 30)
+    return datetime(2014, 9, 30)
 
 
 def time_difference_years(date, from_date):
@@ -62,4 +59,34 @@ def time_difference_years(date, from_date):
     seconds_in_a_year = 60 * 60 * 24 * 365.25
     time_difference_years = time_difference_seconds / seconds_in_a_year
     return time_difference_years
+
+
+def time_difference_years_from_list(dates, from_date):
+    """ Calculated time differences in years for list of dates """
+    vectorized_time_difference_years = np.vectorize(time_difference_years, excluded=['from_date'])
+    time_differences = vectorized_time_difference_years(dates, from_date=from_date)
+    return time_differences
+    
+    
+def datetime_range(start_date, end_date, length):
+    """ Returns a range of #length datetimes starting from start_date and ending at end_date """
+    # Check the type of start_date
+    if isinstance(start_date, str):
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    elif isinstance(start_date, datetime):
+        pass
+    else:
+        raise TypeError("start_date can be either a string with the format %Y-%m-%d or datetime.datetime")
+    # Check the type of end_date
+    if isinstance(end_date, str):
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    elif isinstance(end_date, datetime):
+        pass
+    else:
+        raise TypeError("end_date can be either a string with the format %Y-%m-%d or datetime.datetime")
+                      
+    time_difference = end_date - start_date
+    interval = time_difference / (length - 1)
+    date_range = [start_date + i * interval for i in range(length)]
+    return date_range
 
--- yieldcurve.py
+++ yieldcurve.py
@@ -1,39 +1,96 @@
+import datetime
+import utils
 from nelson_siegel_svensson.calibrate import calibrate_nss_ols
 
+
 class YieldCurve:
+    
+    
     def __init__(self, curve_type):
         """ Initialize parameters """
         self.curve_type = curve_type
         self._data = None
-        self._plot = False
         self._curve = None
         self._status = None
+        self._now = utils.set_now()
+        
         
     def fit(self, market_data, plot=False):
-        """ Fit Nelson-Siegel-Svensson model to the observed yields """
-        # Copy market data for the curve type
+        """ 
+        Fit Nelson-Siegel-Svensson model to the observed yields
+        :param market_data:
+        :param plot:
+        """
         self._data = market_data[market_data['type'] == self.curve_type].copy()
         # Calculate time difference in years between now and expiry
-        self._data['maturity'] = self._data['date'].apply(lambda date: utils.time_difference_years(date, NOW))
+        self._data['maturity'] = self._data['date'].apply(lambda date: utils.time_difference_years(date, self._now))
         # Fit the Nelson-Siegel-Svensson model to our yield data
-        self._curve, self._status = calibrate_nss_ols(np.ravel(data['maturity']), np.ravel(data['rate']))
+        self._curve, self._status = calibrate_nss_ols(np.ravel(self._data['maturity']), np.ravel(self._data['rate']))
+
+        if plot:
+            self._plot_spot_yields(self._data['maturity'], self._data['rate'])
         
-        self._plot = plot
-        if self._plot:
-            self._plot_yield()
         
-    def _plot_yield(self):
-        """ Plot the observed yields and the Nelson-Siegel-Svensson model """
-        # Plot observed yield
-        plt.scatter(self._data['maturity'], self._data['rate'],
-                    label='Observed Yields', marker='o', color='black', s=10)
-        # Plot Nelson-Siegel-Svensson model
-        plt.plot(self._data['maturity'], self._curve(self._data['maturity']),
-                 label='Nelson-Siegel-Svensson', color = 'gray', linestyle='-')
-        # Format the figure
-        plt.title('Yield Curve')
-        plt.xlabel('Maturity time in years')
-        plt.ylabel('Yield in %')
+    def get_spot_yields(self, dates, plot=False):
+        """ Gets the spot yield for dates based on Nelson-Siegel-Svensson model """
+        maturities = utils.time_difference_years_from_list(dates, self._now)
+        yields = self._curve(maturities)
+        if plot:
+            self._plot_spot_yields(dates, yields)
+        return pd.DataFrame({"date": dates, "rate": yields})
+         
+        
+    def _plot_spot_yields(self, times, rates):
+        """ Plots the yields and the Nelson-Siegel-Svensson model vs dates of maturity """ 
+        if isinstance(times[0], datetime.datetime):
+            times_type = 'dates'
+            times_scale = ''
+            continuous_dates = utils.datetime_range(np.min(times), np.max(times), 100)
+            continuous_maturities = utils.time_difference_years_from_list(continuous_dates, self._now)
+            continuous_times = continuous_dates
+        elif isinstance(times[0], float):
+            times_type = 'times'
+            times_scale = ' in years'
+            continuous_maturities = np.linspace(np.min(times), np.max(times), 100)
+            continuous_times = continuous_maturities
+        else:
+            raise TypeError('times can be either iterables of datetime.datetime or float numbers')
+        
+        plt.scatter(times, rates,
+                    label='Spot Yields', marker='o', color='black', s=10)
+        plt.plot(continuous_times, self._curve(continuous_maturities),
+                 label='Nelson-Siegel-Svensson', color='gray', linestyle='-')
+        
+        plt.title(f'Yield Curve vs Maturity {times_type.capitalize()}')
+        plt.xlabel(f'Maturity {times_type}' + times_scale)
+        plt.ylabel('Yield in basis points')
         plt.legend()
-        # Show plot
         plt.show()
+        
+        
+    def get_forward_yields(self, dates, plot=False):
+        """ Gets forward yields """
+        maturities = utils.time_difference_years_from_list(dates, self._now)
+        rates = self._curve(maturities)
+        rates = np.divide(rates, 10000)
+        interests = np.power(np.add(1, rates), maturities)
+        interests_shifted = np.concatenate(([1], np.roll(interests, shift=1)[1:]))
+        forwards = np.multiply(np.add(np.divide(interests, interests_shifted), -1), 10000)
+        
+        if plot:
+            self._plot_forward_yields(dates, forwards)
+            
+        return forwards
+    
+    
+    def _plot_forward_yields(self, dates, rates):
+        """ Plots forward yields """
+        plt.scatter(dates, rates, label='Forward Yields',
+                    marker='o', color='black', s=10)
+        plt.plot(dates, rates, color='gray', linestyle='-')
+        plt.title('Forward Yields vs Maturity Dates')
+        plt.xlabel('Marurity date')
+        plt.ylabel('Yield in basis points')
+        plt.legend()
+        plt.show()
+        
