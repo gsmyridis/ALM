6a5812e1a11ac004d06a3c3d5cb77ff54977672b
georgesmyr <70004539+georgesmyr@users.noreply.github.com>
2023-10-06 17:49:08 +0200

    updated timeutils

--- timeutils.py
+++ timeutils.py
@@ -5,52 +5,6 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 
-def load_portfolio():
-    """
-    :col id: identification number (the number ofthe row)
-    :col account: account type in short
-    :col account_name: long account name
-    
-    :col volume: notional value in EUR
-    :col ir_binging: type of the interest binding (FIX or LIBOR)
-    :col reprice_freq: repricing frequency in months (if the interest binding is LIBOR)
-    :col spread: spread component of the interest rate in basis points
-    
-    - Cash-flow structure of the products
-    :col issue: issue date (this is the first repricing day)
-    :col maturity: maturity date
-    :col repayment: type of principal repayment structure (bullet, linear, or annuity)
-    :col payment_freq: repayment frequency in number of months
-    :col yieldcurve: identifier of the interest rate curve
-    """
-    portfolio = pd.read_csv('data/portfolio.csv')
-    portfolio.set_index('id', inplace=True)
-    portfolio['issue'] = pd.to_datetime(portfolio['issue'])
-    portfolio['maturity'] = pd.to_datetime(portfolio['maturity'])
-    portfolio['reprice_freq'] = portfolio['reprice_freq'].fillna(1).astype(int)
-
-    return portfolio
-
-
-def load_market():
-    """
-    :col type: yield curve type (for example, yields are from the bond market or the interbank market)
-                the type column has to be the same as in portfolio to connect the two datasets
-    :col date: maturity of the current rate
-    :col rate: value of the rate in basis points
-    :col comment: label of the yield curve tensor
-    """ 
-    market = pd.read_csv('data/market.csv')
-    market['date'] = pd.to_datetime(market['date'])
-    market['rate'] = market['rate']
-    return market
-
-
-def load_data():
-    """ Loads the portfolio and market data """
-    return load_portfolio(), load_market()
-
-
 def set_today(date=datetime(2014,9,30)):
     """ Set today's date """
     if not isinstance(date, (datetime, str)):
